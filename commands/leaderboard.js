13f45db97f27724a9689c0dae9588edd439aea39
= <reidluna@icloud.com>
2018-05-30 22:24:24 -0600

    Finished Leaderboard Function

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,83 +1,173 @@
-const table = require("text-table");
+// console.table is how we format the leaderboard
 const ctable = require("console.table");
 
-exports.run = async (client, message) => {
-  // set headers for table
-  let tableArr = [["pos", "user", "points", "level"]];
+// this function will get usernames (nicknames) from an array of user ids
+let getUsernames = async (userArray, message) => {
+  // establishing the array that we will return
+  let returnARR = [];
 
-  // get key and value for all users
+  // a for loop for every item in the user array
+  userArray.forEach(async userId => {
+    // gets the user's collection
+    let userCollection = await message.guild.fetchMember(userId);
+
+    // sets the nickname for the user
+    let username = userCollection.displayName;
+
+    // pushes to the return array
+    returnARR.push(username);
+  });
+
+  // returns the array
+  return returnARR;
+};
+
+// this function will combine keys and data together
+let combine = async (usernames, data) => {
+  // establishing the array we will return
+  let returnARR = [];
+
+  // a for loop for every user in the usernames (key) array
+  for (i = 0; i < usernames.length; i++) {
+    // sets the object for the user
+    let obj = {
+      // username / nickname
+      username: usernames[i],
+      // position
+      pos: null,
+      // points
+      points: data[i].points,
+      // level
+      level: data[i].level
+    };
+    // pushes previous obj to the return array
+    returnARR.push(obj);
+  }
+
+  // returns the array
+  return returnARR;
+};
+
+// this function will sort the users by score and set a position
+let sort = unsorted => {
+  // will sort by points highest to lowest
+  // P.S. i know it should be a-b but that didnt work
+  let sorted = unsorted.sort((a, b) => {
+    return b.points - a.points;
+  });
+
+  // for loop for every item in the sorted array to set the position
+  for (i = 0; i < sorted.length; i++) {
+    sorted[i].pos = i + 1;
+  }
+
+  // returns the sorted array
+  return sorted;
+};
+
+// makes a table for the leaderboard
+let makeTable = async (sorted, setLimit, args, message) => {
+  // establish the table that we are returning
+  let ReturnTable = null;
+
+  // establish the array of objects for the table
+  let tableArr = [];
+
+  // if there are arguments we will show a specific user
+  if (args) {
+    // if the args starts with a < then we know it is a mention, therefore a user
+    // ex: <@!261567666102206464>
+    if (args.startsWith("<")) {
+      // we just want the numbers from the menton for the user id
+      args = args.replace(/[^0-9.]/g, "");
+
+      // get the user and the nickname for the user
+      let user = await message.guild.fetchMember(args);
+      let nickname = user.displayName;
+
+      // loop through the sorted array of users to get a specific user
+      for (i = 0; i < sorted.length; i++) {
+        // using deconstruction to set the user's variables
+        let { pos, username, points, level } = sorted[i];
+
+        // if the username in the sorted array matches the user we got from args
+        // then set the obj to that specific user and output the table array
+        if (username === nickname) {
+          let obj = {
+            pos: pos,
+            username: username,
+            points: points,
+            level: level
+          };
+          tableArr.push(obj);
+        }
+      }
+    } else {
+      // if the args is just a number, then it will show the user with the position
+      // specified. ex: +board 1 will show the first user
+      for (i = 0; i < sorted.length; i++) {
+        // using deconstruction like before and if the args match the position
+        // then push the user to the table array
+        let { pos, username, points, level } = sorted[i];
+        if (pos === parseInt(args)) {
+          let obj = {
+            pos: pos,
+            username: username,
+            points: points,
+            level: level
+          };
+          tableArr.push(obj);
+        }
+      }
+    }
+  } else {
+    // if there are no arguments, set the limit to either the limit specified
+    // or the length of the sorted array
+    let limit = setLimit > sorted.length ? sorted.length : setLimit;
+
+    // look at me using deconstruction again, but this time we are pushing all
+    // users to the table
+    for (i = 0; i < limit; i++) {
+      let { pos, username, points, level } = sorted[i];
+      let obj = {
+        pos: pos,
+        username: username,
+        points: points,
+        level: level
+      };
+      tableArr.push(obj);
+    }
+  }
+  // we then return the table to the caller
+  ReturnTable = ctable.getTable(tableArr);
+  return ReturnTable;
+};
+
+exports.run = async (client, message, args) => {
+  // start the array for the table (Leaderboard Array)
+  let tableArr = null;
+
+  // get key and value for all users points and levels
   let pointsKeyArray = client.points.keyArray();
   let pointsArray = client.points.array();
 
   // convert keys into usernames
-  let getUsernames = async userArray => {
-    let returnARR = [];
-    userArray.forEach(async userId => {
-      let userCollection = await message.guild.fetchMember(userId);
-      let username = userCollection.user.username;
-      returnARR.push(username);
-    });
-    return returnARR;
-  };
-  let usernames = await getUsernames(pointsKeyArray);
+  let usernames = await getUsernames(pointsKeyArray, message);
 
-  // combine users and data
-  let combine = async (usernames, data) => {
-    let returnARR = [];
-    for (i = 0; i < usernames.length; i++) {
-      returnARR[i] = {
-        username: usernames[i],
-        pos: null,
-        points: data[i].points,
-        level: data[i].level
-      };
-    }
-    return returnARR;
-  };
+  // combine usernames with points and levels
   let unsortedScores = await combine(usernames, pointsArray);
 
   // sort out positions
-  let sort = unsorted => {
-    let sorted = unsorted.sort((a, b) => {
-      return b.points - a.points;
-    });
-    for (i = 0; i < sorted.length; i++) {
-      sorted[i].pos = i + 1;
-    }
-    return sorted;
-  };
   let sortedScores = sort(unsortedScores);
 
-  let makeTable = sorted => {
-    for (i = 0; i < sorted.length; i++) {
-      let { pos, username, points, level } = sorted[i];
-      tableArr.push([pos, username, points, level]);
-    }
-  };
-  makeTable(sortedScores);
-  console.log(tableArr);
-  let leaderboard = table(tableArr);
-  message.channel.send(leaderboard, {
-    code: "asciidoc",
-    split: { char: "\u200b" }
-  });
+  // make the leaderboard table. with the sorted scores, a limit of ten, pass
+  // in the args from the message, and the message object
+  let leaderboard = await makeTable(sortedScores, 10, args[0], message);
 
-  let newLeader = ctable.getTable([
-    {
-      pos: 1,
-      user: "reid",
-      points: 10,
-      level: 1
-    },
-    {
-      pos: 1,
-      user: "snowman",
-      points: 10,
-      level: 1
-    }
-  ]);
-  message.channel.send(newLeader, {
-    code: "asciidoc",
+  // send the leaderboard to the user
+  message.channel.send(leaderboard, {
+    // this code is markdown
+    code: "md",
     split: { char: "\u200b" }
   });
 };
@@ -95,5 +185,5 @@ exports.help = {
   usage: "leaderboard [username or blank]",
   category: "Points & Level",
   extended:
-    "if the space after leaderboard is left blank, then it will default to the top ten, if it has a user then it will show their position"
+    "if the space after leaderboard is left blank, then it will default to the top ten, if it has a user or a position then it will show the specific user"
 };
--- modules/functions.js
+++ modules/functions.js
@@ -182,9 +182,7 @@ module.exports = client => {
     process.exit(1);
   });
 
-  /*
   process.on("unhandledRejection", err => {
     client.logger.error(`Unhandled rejection: ${err}`);
   });
-  */
 };
